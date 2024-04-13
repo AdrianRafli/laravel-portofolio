1f76997dc8f6bc09326c6f302dd3792f218f03e6
adrianrafli <adrianrafly20@gmail.com>
2024-04-13 12:39:06 +0700

    add model and migration

--- database/migrations/2024_04_12_185908_create_projects_table.php
+++ database/migrations/2024_04_12_185908_create_projects_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('image');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['admin', 'user'])->default('user');
-            $table->rememberToken();
+            $table->text('description');
+            $table->string('tech_stack');
+            $table->string('github_link');
+            $table->string('demo_link');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('projects');
     }
 };
